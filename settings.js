4e2b6fb919e6ae1cb3210833ccadce936791c289
elmyrzatagaybekov-max <elmyrzatagaybekov@gmail.com>
2026-02-04 14:35:45 +0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'ariet bot', // Your name
-  ownerNumber: '997646425', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '996997646425', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "private",
   maxStoreMessages: 20, 
